0ff57aa01f6c50763f19bc24cf91fa6b6173cd25
Zakhar Kleyman <zakhar.kleyman@mongodb.com>
2020-06-10 10:24:18 -0400

    feat(BUILD-11337): add swift toolchain for ubuntu 16.04 (#1)
    * feat(BUILD-11337): add swift toolchain for ubuntu 16.04
    
    * add ansible-role-toolchain to requirements

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,5 +7,14 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_host(host):
-    assert host.file("/etc/hosts").exists
+def test_swift_toolchain(host):
+    revision = "4.1-RELEASE"
+    assert host.file("/opt/swift").exists
+    assert host.file("/opt/swift/toolchain_version").exists
+    assert host.file("/opt/swift/toolchain_version").contains(revision)
+
+
+def test_swift_symlink(host):
+    link_path = "/opt/swift-4.1-RELEASE-ubuntu16.04"
+    assert host.file(link_path).is_symlink
+    assert host.file(link_path).linked_to == "/opt/swift"
